58f86f3847d4c500d196d0223bb595d0ac7e1728
mumu-tw <080@gimu.com.tw>
2026-04-10 17:32:41 +0800

    Update blog.ts

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -57,7 +57,7 @@ const getNormalizedPost = async (post: CollectionEntry<'post'>): Promise<Post> =
     metadata = {},
   } = data;
 
-  const slug = cleanSlug(id); // cleanSlug(rawSlug.split('/').pop());
+  const slug = cleanSlug(id.replace(/\/index$/, '')); // cleanSlug(rawSlug.split('/').pop());
   const publishDate = new Date(rawPublishDate);
   const updateDate = rawUpdateDate ? new Date(rawUpdateDate) : undefined;
 
